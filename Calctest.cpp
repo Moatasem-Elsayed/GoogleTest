0a2d3671543ec056444b6414b5d071e0bee3722c
Moatasem-Elsayed <eng.moatasem.8@gmail.com>
2021-12-21 12:05:57 +0200

    Lab #Video3

--- Calctest.cpp
+++ Calctest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "Calc.hpp"
+#include <typeinfo>
 
 TEST(SumFunction, Test_1)
 {
@@ -15,10 +16,7 @@ TEST(SubFunction, Test_1)
 {
     EXPECT_EQ(-1, sub(2, 3));
 }
-TEST(SubFunction, Test_2)
-{
-    EXPECT_NE(8, sum(12, 3));
-}
+
 int main()
 {
     ::testing::InitGoogleTest();
